542c1b4bb5cb966934a58094147e3996d2726a68
MaxGalindo150 <maximilianogalindo7@gmail.com>
2021-12-28 16:05:59 -0600

    We can obtain just a word from data.txt

--- hangman.py
+++ hangman.py
@@ -1,7 +1,13 @@
+import random
+
 def run():
     with open("./data.txt", "r", encoding = "utf-8") as d:
         words = [word for word in d]
-        print(words)
+
+    j = random.randint(0,170)
+    for i, words in enumerate(words):
+        if i == j:
+            print(words)
 
 if __name__ == '__main__':
     run()
