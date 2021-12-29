3c48d120422820bf0df01f5bf0d5d147bdb63ca7
MaxGalindo150 <maximilianogalindo7@gmail.com>
2021-12-28 18:14:51 -0600

    We use functions

--- hangman.py
+++ hangman.py
@@ -1,15 +1,29 @@
 import random
 
-def run():
+def read_words(): #read data
     with open("./data.txt", "r", encoding = "utf-8") as d:
-        words = [word for word in d]
+        list_words = [word for word in d]
+    return list_words
 
+def size(): #Obtain the number of letters of just a word
+    words = read_words()
     j = random.randint(0,170)
-    for i, words in enumerate(words):
-        if i == j:
-            word = words
-    l = len(word)
-    print(l*" _ ")
+    l = len(words[j]) - 1
+    return l
+      
+
+def run():
+     print(size()*"_ ")
+     
+    #with open("./data.txt", "r", encoding = "utf-8") as d:
+     #   words = [word() for word in d]
+
+    #j = random.randint(0,170)
+    #for i, words in enumerate(words):
+    #    if i == j:
+    #        word = words
+    #l = len(word)
+    #print(l*" _ ")
 
 if __name__ == '__main__':
     run()
