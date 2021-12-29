b5c9079eefd05b0c7bdf56b70f6d61173b6311e6
MaxGalindo150 <maximilianogalindo7@gmail.com>
2021-12-28 21:35:47 -0600

    it works but details are missing

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 def read_words(): #read data
     with open("./data.txt", "r", encoding = "utf-8") as d:
@@ -8,22 +9,30 @@ def read_words(): #read data
 def size(): #Obtain the number of letters of just a word
     words = read_words()
     j = random.randint(0,170)
-    l = len(words[j]) - 1
-    return l
+    s = len(words[j]) - 1
+    return s, words[j]
       
 
 def run():
-     print(size()*"_ ")
+     s, word = size()
+     print("\nGuess the word!\n")
      
-    #with open("./data.txt", "r", encoding = "utf-8") as d:
-     #   words = [word() for word in d]
+     unders = s*"_" + "\n"
+     print(unders)
+     
+     letters = list(unders)
 
-    #j = random.randint(0,170)
-    #for i, words in enumerate(words):
-    #    if i == j:
-    #        word = words
-    #l = len(word)
-    #print(l*" _ ")
-
+     while unders != word:
+        l = input("\nEnter a letter: ")
+        for i in range(0,s):
+            if l == word[i]:
+                letters[i] = l         
+        os.system("clear")
+        unders = "".join(letters)
+        print("\nGuess the word!\n")
+        print(unders.upper())
+     
+        
+     
 if __name__ == '__main__':
     run()
